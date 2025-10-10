a43512bacc656b7ed2ff3a304b64ede375d76dbc
Jewel Rana <ranaausasiagroup@gmail.com>
2025-10-10 15:18:24 +1100

    Update home-1 landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,25 @@
-import AmIEligibleSection from "@/components/AmIEligibleSection";
-import ContactSection from "@/components/ContactSection";
-import FAQSection from "@/components/FaqSection";
-import Footer from "@/components/Footer";
-import HeroFooter from "@/components/HeroFooter";
-import HeroSection from "@/components/HeroSection";
-import Navbar from "@/components/Navbar";
-import OurBlogSection from "@/components/OurBlogSection";
-import ServicesSection from "@/components/ServicesSection";
-import TestimonialsSection from "@/components/TestimonialsSection";
+import AmIEligibleSection from "@/components/home-1/AmIEligibleSection";
+import ContactSection from "@/components/home-1/ContactSection";
+import FAQSection from "@/components/home-1/FaqSection";
+import Footer from "@/components/home-1/Footer";
+import HeroSection from "@/components/home-1/HeroSection";
+import Navbar from "@/components/home-1/Navbar";
+import OurBlogSection from "@/components/home-1/OurBlogSection";
+import ServicesSection from "@/components/home-1/ServicesSection";
+import TestimonialsSection from "@/components/home-1/TestimonialsSection";
 
 export default function Home() {
   return (
-   <div>
-    <Navbar />
-    <HeroSection />
-    
-    {/* <HeroFooter/> */}
-    <ServicesSection />
-    <TestimonialsSection/>
-    <OurBlogSection />
-    <AmIEligibleSection />
-    <ContactSection />
-    <FAQSection />
-    <Footer />
-   </div>
+    <div>
+      <Navbar />
+      <HeroSection />
+      <ServicesSection />
+      <TestimonialsSection />
+      <OurBlogSection />
+      <AmIEligibleSection />
+      <ContactSection />
+      <FAQSection />
+      <Footer />
+    </div>
   );
 }
